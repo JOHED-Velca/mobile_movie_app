168a83574d26808105a3b1c9eccd9e0653e99a3d
Jonathan Velasquez <johedvelca@gmail.com>
2025-05-22 22:16:53 -0400

    TabIcon component created and styled

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,19 @@
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
-import React from 'react'
+import { Image, ImageBackground, Text } from 'react-native'
+
+const TabIcon = () => {
+    return (
+        <ImageBackground
+            source={images.highlight}
+            className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+        >
+            <Image source={icons.home} tintColor="#151312" className="size-5" />
+            <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+        </ImageBackground>
+    )
+}
 
 const _Layout = () => {
   return (
@@ -9,6 +23,9 @@ const _Layout = () => {
             options={{
                 title: 'Home',
                 headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon />
+                )
             }}
         />
         <Tabs.Screen
