7e922f6991c12daca3d05384e218580215915623
Jonathan Velasquez <johedvelca@gmail.com>
2025-05-26 18:25:35 -0400

    statusBar (clock and battery) hidden.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,26 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen
-      name="(tabs)"
-      options={{
-        headerShown: false,
-      }}
-    />
-    <Stack.Screen
-      name="movies/[id]"
-      options={{
-        headerShown: false,
-      }}
-    />
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
 
-  </Stack>
+      </Stack>
+    </>
+  )
 }
